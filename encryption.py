1e544bfb87cb8f3cf451ab77d5720ec0fb777b2d
Vijayaraghavan <vijayaraghavan1453@gmail.com>
2022-03-09 21:57:37 +0530

    Symmetric key encryption

--- encryption.py
+++ encryption.py
@@ -1,18 +1,37 @@
 import time
+import sys
+import base64
 from cryptography.fernet import Fernet
 
-key = Fernet.generate_key()   ##Key Generation
+def key_generation(message):
+    key = Fernet.generate_key()   ##Key Generation
+    fernet=Fernet(key)
+    return fernet 
 
-t=Fernet(key)  ## Symmetric Key 
+def encrypt():
+    message = input("Enter your message to encrypt: ")
+    path = input("Enter a file to store symmetric key:(/root/enc.txt) ")
+    if not message or not path:
+        sys.exit("No message/path provided..")
 
-message="Hello! how are you?" ## Message to encrypt
+    encrypt=key_generation(message).encrypt(message.encode())
+    print(encrypt)
+    try:
+        with open(path,"wb") as file:
+            file.write(encrypt)
+    except:
+        sys.exit("Check your filename and file permissions")
+    
+    
+def main():
+    opt=int(input("Main Menu\n1.Encrypt.\n2.Decrypt\n0.Exit."))
+    if opt == 1:
+        encrypt()
+    elif opt == 2:
+        sys.exit("Will be added on next update") #decrypt()
+    elif opt == 0:
+        quit()
+    else:
+        sys.exit("Kindly select a valid option...")
 
-cipher=t.encrypt(message.encode())  ##Encodes to bytes inorder to encrypt
-
-print("\n++++++++++++++++  Encryption ++++++++++++++++\n")
-print(cipher.decode())
-
-time.sleep(3)
-print("\n++++++++++++++++  Decryption ++++++++++++++++\n")
-print(t.decrypt(cipher).decode(),"\n")
-
+main()
